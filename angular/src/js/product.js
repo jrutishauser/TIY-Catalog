886d210e1b89b0934dbc9440785c0959cb486729
Jon Rutishauser <jrutishauser@gmail.com>
2015-03-06 01:33:10 -0500

    added the variation menus and a few other things

--- angular/src/js/product.js
+++ angular/src/js/product.js
@@ -2,8 +2,19 @@
     angular.module('tiy-catalog', [])
         .controller('ProductController', function () {
             this.prodArray = [1, 2, 3, 4, 5, 6];
+			this.variationuno = false;
+			this.variationdos = false;	
 			this.moused = function(){
-				console.log('mouseover');
+				this.variationuno = true;
+			};
+			this.unmoused = function(){
+				this.variationuno = false;	
+			};
+			this.moused2 = function(){
+				this.variationdos = true;
+			};
+			this.unmoused2 = function(){
+				this.variationdos = false;	
 			};
 			this.pics = [
 			'http://www.q-tees.co.uk/image/cache/data/t-shirts2/html%20green-542x700.jpg',
@@ -11,7 +22,6 @@
 			];
 			this.imgnum = 0;
        		this.morepics = function(){
-				console.log('clicked!');
 				this.imgnum++;
 				if (this.imgnum === this.pics.length){
 					this.imgnum = 0;	
